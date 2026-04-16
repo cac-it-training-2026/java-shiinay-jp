3c371ee6b7591aa49c0f35cb66789974f6cb0ab2
shiinay <shiinay@C010423280.zipangu.cac.co.jp>
2026-04-16 10:34:54 +0900

    Q1-1完了

--- java_comprehension_exercises_volume1/src/basic/question01/Question01_1.java
+++ java_comprehension_exercises_volume1/src/basic/question01/Question01_1.java
@@ -8,7 +8,25 @@ public class Question01_1 {
 
 	public static void main(String[] args) {
 
-		// TODO:ここに実装
+		//１．intがたの変数[price]を初期化して作成する。
+		int price = 120;
+
+		//２．Stringがたの変数[item]を初期化して作成する。
+		String item = "ノート";
+
+		//３．[price、[item]の順番で値を次の通りコンソールに出力する。
+		System.out.println(price);
+		System.out.println(item + "\n");
+
+		//４．[price]に値を代入する。
+		price = 200;
+
+		//５．[item]に値を代入する。
+		item = "鉛筆";
+
+		//６．[price]、[item]の順番で値を次の通りコンソールに出力する。
+		System.out.println(price);
+		System.out.println(item);
 
 	}
 
