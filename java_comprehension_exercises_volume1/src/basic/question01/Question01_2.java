40bfa6e89c0290bee34cd4b409ccd8844f6e550d
shiinay <shiinay@C010423280.zipangu.cac.co.jp>
2026-04-16 10:35:13 +0900

    Q1-2完了

--- java_comprehension_exercises_volume1/src/basic/question01/Question01_2.java
+++ java_comprehension_exercises_volume1/src/basic/question01/Question01_2.java
@@ -7,9 +7,23 @@ package basic.question01;
 public class Question01_2 {
 
 	public static void main(String[] args) {
-		
-		// TODO:ここに実装
 
+		//１．int 型の変数 [applePrice] を初期化して作成する
+		int applePrice = 100;
+
+		//２．String 型の変数[stationery] を初期化して作成する
+		String stationery = "鉛筆";
+
+		//３．int 型の変数[bananaPrice]を初期化して作成する
+		int bananaPrice = applePrice;
+
+		//４．String 型の変数[writingUtensils]を初期化して作成する
+		String writingUtensils = stationery;
+
+		//５． [applePrice]、[bananaPrice]、[writingUtensils]の順番で値を次のとおりコンソールに出力する
+		System.out.println("りんごの値段は" + applePrice + "円です。");
+		System.out.println("バナナの値段は" + bananaPrice + "円です。");
+		System.out.println("この筆記用具は" + writingUtensils + "です。");
 	}
 
 }
