d17f7939a05e8e81444b425a1e218a64bead4847
shiinay <shiinay@C010423280.zipangu.cac.co.jp>
2026-04-16 14:19:35 +0900

    Q7-3完了

--- java_comprehension_exercises_volume1/src/advanced/question07/Question07_3.java
+++ java_comprehension_exercises_volume1/src/advanced/question07/Question07_3.java
@@ -1,6 +1,8 @@
 package advanced.question07;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question07_3 {
 
@@ -14,7 +16,58 @@ public class Question07_3 {
 		int[] amounts = new int[5];
 
 		// TODO: 実装ここから
+		int[] stocks = { 1, 2, 3, 4, 5 };
 
+		int sum = 0;
+		int num = 0;
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		for (int i = 0; i < itemNames.length; i++) {
+
+			System.out.println(itemNames[i] + "の在庫数：＞" + stocks[i]);
+
+		}
+
+		for (int i = 0; i < itemNames.length; i++) {
+
+			while (num < stocks[i]) {
+
+				System.out.print(itemNames[i] + "の購入数：＞");
+				String res = reader.readLine();
+				num = Integer.parseInt(res);
+
+				if (num > stocks[i]) {
+
+					System.out.println("在庫を超えています。もう一度入力してください。");
+
+				} else {
+
+					amounts[i] = num;
+					stocks[i] -= num;
+					num = 0;
+
+					break;
+
+				}
+
+			}
+
+		}
+
+		for (int i = 0; i < itemNames.length; i++) {
+
+			System.out.println(itemNames[i] + "：購入" + amounts[i] + "個");
+			System.out.println("在庫" + stocks[i] + "個");
+
+		}
+
+		for (int i = 0; i < itemNames.length; i++) {
+			sum += amounts[i] * prices[i];
+		}
+
+		System.out.println("合計金額：" + sum + "円");
 
 	}
+
 }
